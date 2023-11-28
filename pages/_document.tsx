767ca32d9241c787bf56071cab3dc80a839ec20f
Maxdeoro <maxdeoro@gmail.com>
2023-11-28 23:03:07 +0300

    modified _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,39 @@
-import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
-}
+import { Html, Head, Main, NextScript, DocumentContext,DocumentInitialProps } from 'next/document';
+import Document from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {...initialProps};
+  };
+
+  render(): JSX.Element {
+    return (
+      <Html lang='en'>
+        <Head/>
+        <body>
+          <Main/>
+          <NextScript/>
+        </body>
+      </Html>
+    );
+  };
+};
+export default MyDocument;
+
+// export default function Document() {
+
+
+//   return (
+//     <Html lang="en">
+//       <Head />
+//       <body>
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   )
+// }
+
+
